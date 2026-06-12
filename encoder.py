7808fa5df89cee7db71352b07bf81fe085ced7b4
caipherteam2 <caipherteam2@gmail.com>
2026-06-12 22:17:46 +0530

    Create encoder.py placeholder

---
+++ encoder.py
@@ -0,0 +1,5 @@
+'''
+Place your encoding logic in the following funciton, ensuring not to cross double the size of the orignal message within your encoded message. 
+'''
+def encoder(message:str) -> bytes | int:
+  pass
